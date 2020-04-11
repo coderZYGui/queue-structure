ccf39b7d26eba6712c36bde79b559491dc37d018
zygui_g <zygui@163.com>
2020-04-11 21:11:37 +0800

    初始化队列,并设计队列接口

---
+++ queue/src/com/zy/Main.java
@@ -0,0 +1,10 @@
+package com.zy;
+
+/**
+ * Description:
+ *
+ * @author zygui
+ * @date 2020/4/11 21:07
+ */
+public class Main {
+}
---
+++ queue/src/com/zy/Queue.java
@@ -0,0 +1,39 @@
+package com.zy;
+
+/**
+ * Description: 队列的实现(根据双向链表实现)
+ *
+ * @author zygui
+ * @date 2020/4/11 21:08
+ */
+public class Queue<E> {
+
+    // 元素的数量
+    public int size() {
+        return 0;
+    }
+
+    // 队列是否为空
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // 入队
+    public void enQueue(E element) {
+
+    }
+
+    // 出队
+    public E deQueue() {
+        return null;
+    }
+
+    // 清空队列
+    public E front() {
+        return null;
+    }
+
+    public void clear() {
+
+    }
+}
